9d9e0edb5b0d2d6f221d8a0a59259c670cec5601
Jason Salaber <jcsalaber@hotmail.com>
2021-11-26 14:49:29 -0500

    feat: added builder to client and user to simplify use of sdk

--- DevCycle/DVCClient.swift
+++ DevCycle/DVCClient.swift
@@ -7,6 +7,8 @@
 enum ClientError: Error {
     case NotImplemented
     case BuiltClient
+    case InvalidEnvironmentKey
+    case InvalidUser
 }
 
 import Foundation
@@ -17,6 +19,14 @@ public class DVCClient {
     
     init(environmentKey: String? = nil, user: DVCUser? = nil) {
         self.environmentKey = environmentKey
+        self.user = user
+    }
+    
+    fileprivate func setEnvironmentKey(environmentKey: String) {
+        self.environmentKey = environmentKey
+    }
+    
+    fileprivate func setUser(user: DVCUser) {
         self.user = user
     }
     
@@ -46,5 +56,43 @@ public class DVCClient {
 
     public func flushEvents() throws -> String {
         throw ClientError.NotImplemented
+    }
+    
+    public class ClientBuilder {
+        private var client: DVCClient
+        
+        init() {
+            self.client = DVCClient()
+        }
+        
+        func environmentKey(key: String) -> ClientBuilder {
+            self.client.setEnvironmentKey(environmentKey: key)
+            return self
+        }
+        
+        func user(user: DVCUser) -> ClientBuilder {
+            self.client.setUser(user: user)
+            return self
+        }
+        
+        func build() -> DVCClient? {
+            if (self.client.environmentKey == nil) {
+                print("Missing Environment Key")
+                return nil
+            }
+            
+            if (self.client.user == nil) {
+                print("Missing User")
+                return nil
+            }
+                    
+            let result = self.client
+            self.client = DVCClient()
+            return result
+        }
+    }
+    
+    static func builder() -> ClientBuilder {
+        return ClientBuilder()
     }
 }
--- DevCycle/DVCUser.swift
+++ DevCycle/DVCUser.swift
@@ -8,7 +8,7 @@ import Foundation
 
 public class DVCUser {
     public var userId: String?
-    public var isAnonymous: Bool
+    public var isAnonymous: Bool?
     public var email: String?
     public var name: String?
     public var language: String?
@@ -26,12 +26,85 @@ public class DVCUser {
     public let sdkVersion: String
     
     init() {
-        self.isAnonymous = true
         self.createdDate = Date()
         self.platform = "iOS"
         self.platformVersion = "0.0.1"
         self.deviceModel = "iPhone"
         self.sdkType = "client"
         self.sdkVersion = "0.0.1"
+    }
+    
+    class UserBuilder {
+        var user: DVCUser
+        
+        init() {
+            self.user = DVCUser()
+        }
+        
+        func userId(userId: String) -> UserBuilder {
+            self.user.userId = userId
+            self.user.isAnonymous = false
+            return self
+        }
+        
+        func isAnonymous(isAnonymous: Bool) -> UserBuilder {
+            self.user.isAnonymous = isAnonymous
+            self.user.userId = "random_id" // TODO: Create random user id
+            return self
+        }
+        
+        func email(email: String) -> UserBuilder {
+            self.user.email = email
+            return self
+        }
+        
+        func name(name: String) -> UserBuilder {
+            self.user.name = name
+            return self
+        }
+        
+        func language(language: String) -> UserBuilder {
+            self.user.language = language
+            return self
+        }
+        
+        func country(country: String) -> UserBuilder {
+            self.user.country = country
+            return self
+        }
+        
+        func appVersion(appVersion: String) -> UserBuilder {
+            self.user.appVersion = appVersion
+            return self
+        }
+        
+        func appBuild(appBuild: Int) -> UserBuilder {
+            self.user.appBuild = appBuild
+            return self
+        }
+        
+        func customData(customData: [String:Any]) -> UserBuilder {
+            self.user.customData = customData
+            return self
+        }
+        
+        func publicCustomData(publicCustomData: [String:Any]) -> UserBuilder {
+            self.user.publicCustomData = publicCustomData
+            return self
+        }
+        
+        func build() -> DVCUser? {
+            guard let _ = self.user.userId, let _ = self.user.isAnonymous else {
+                return nil
+            }
+            
+            let result = self.user
+            self.user = DVCUser()
+            return result
+        }
+    }
+    
+    static func builder() -> UserBuilder {
+        return UserBuilder()
     }
 }
