2b6e86ecc989ef8610b843832a74a294b54b206c
Jason Salaber <jcsalaber@hotmail.com>
2021-11-26 14:49:29 -0500

    feat: added builder to client and user to simplify use of sdk

--- DevCycleTests/DVCClientTest.swift
+++ DevCycleTests/DVCClientTest.swift
@@ -17,4 +17,23 @@ class DVCClientTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         
     }
+    
+    func testBuilderReturnsNilIfNoEnvKey() {
+        let user = DVCUser.builder()
+                    .userId(userId: "my_user")
+                    .build()!
+        XCTAssertNil(DVCClient.builder().user(user: user).build())
+    }
+    
+    func testBuilderReturnsNilIfNoUser() {
+        XCTAssertNil(DVCClient.builder().environmentKey(key: "my_env_key").build())
+    }
+    
+    func testBuilderReturnsClient() {
+        let user = DVCUser.builder()
+                    .userId(userId: "my_user")
+                    .build()!
+        let client = DVCClient.builder().user(user: user).environmentKey(key: "my_env_key").build()
+        XCTAssertNotNil(client)
+    }
 }
