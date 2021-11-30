bacaf84db1e22a226c843681b7a4d1b677ce1957
Jason Salaber <jcsalaber@hotmail.com>
2021-11-30 17:04:09 -0500

    feat: added service file to make requests

---
+++ DevCycle/Models/DVCConfig.swift
@@ -0,0 +1,14 @@
+//
+//  DVCConfig.swift
+//  DevCycle
+//
+//  Created by Jason Salaber on 2021-11-30.
+//
+
+import Foundation
+
+public struct DVCConfig {
+    var environmentKey: String
+    var user: DVCUser
+    var config: Any?
+}
