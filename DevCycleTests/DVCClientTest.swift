9ac3c1194769aedd3655f6e11abab8c9b881a8ac
Jason Salaber <jcsalaber@hotmail.com>
2021-12-03 14:03:05 -0500

    chore: remove tear down and setup code from tests to fix fastlane tests

--- DevCycleTests/DVCClientTest.swift
+++ DevCycleTests/DVCClientTest.swift
@@ -8,16 +8,6 @@ import XCTest
 @testable import DevCycle
 
 class DVCClientTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        
-    }
-    
     func testBuilderReturnsNilIfNoEnvKey() {
         let user = DVCUser.builder()
                     .userId("my_user")
--- DevCycleTests/DVCUserTest.swift
+++ DevCycleTests/DVCUserTest.swift
@@ -8,15 +8,6 @@ import XCTest
 @testable import DevCycle
 
 class DVCUserTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
     func testCreateUser() {
         let user = DVCUser()
         XCTAssert(user.platform == "iOS")
