0db30333e0004f1e041107ad59fd820cf1516867
Jason Salaber <jcsalaber@hotmail.com>
2021-12-03 14:03:05 -0500

    chore: remove tear down and setup code from tests to fix fastlane tests

--- DevCycleTests/DevCycleServiceTests.swift
+++ DevCycleTests/DevCycleServiceTests.swift
@@ -8,15 +8,6 @@ import XCTest
 @testable import DevCycle
 
 class DevCycleServiceTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
     func testCreateGetURLRequest() throws {
         let url = getService().createConfigRequest(user: getTestUser()).url?.absoluteString
         XCTAssert(url!.contains("envKey=my_env_key"))
