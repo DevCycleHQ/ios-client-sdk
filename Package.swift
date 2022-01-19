49254643317cbe7c9ca7cd1f1e0a8d5410d0d913
Jason Salaber <jcsalaber@hotmail.com>
2022-01-19 12:32:31 -0500

    chore: added swift package file for spm

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "DevCycle",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_12)
+    ],
+    products: [
+        .library(
+            name: "DevCycle",
+            targets: ["DevCycle"]),
+    ],
+    targets: [
+        .target(
+            name: "DevCycle",
+            path: "DevCycle"
+        ),
+        .testTarget(
+            name: "DevCycleTests",
+            dependencies: [
+                "DevCycle",
+            ],
+            path: "DevCycleTests",
+            exclude: ["ObjC"]
+        ),
+        .testTarget(
+            name: "DevCycleTests-ObjC",
+            dependencies: [
+                "DevCycle",
+            ],
+            path: "DevCycleTests/ObjC"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
+
