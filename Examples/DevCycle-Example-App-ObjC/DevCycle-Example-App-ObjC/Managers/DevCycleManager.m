62dfc3628b3aeaeaa809dfd5277a7f49172e000f
Jonathan Norris <jonathan@taplytics.com>
2022-01-25 11:45:26 -0500

    DVC-1956 README.md updates for Objective-C

--- Examples/DevCycle-Example-App-ObjC/DevCycle-Example-App-ObjC/Managers/DevCycleManager.m
+++ Examples/DevCycle-Example-App-ObjC/DevCycle-Example-App-ObjC/Managers/DevCycleManager.m
@@ -42,7 +42,11 @@ static NSString *const DEVELOPMENT_KEY = @"mobile-16e8e500-80d2-4bb5-9d4f-821938
     self.client = [DVCClient initialize:DEVELOPMENT_KEY
                                    user:user
                                 options:options
-                          onInitialized:onInitialized];
+                          onInitialized:^(NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"DevCycle failed to initialize: %@", error);
+        }
+    }];
     if (err) {
         NSLog(@"Error Starting DevCycle: %@", err);
     }
