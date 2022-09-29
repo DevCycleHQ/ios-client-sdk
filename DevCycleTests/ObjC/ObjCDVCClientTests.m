c2e0ac3dd84b6fd948c65a6454b7137efb0140f6
Jason Salaber <jcsalaber@hotmail.com>
2022-09-29 14:43:54 -0400

    fix: fixed the objc tests that failed the request consolidator tests

--- DevCycleTests/ObjC/ObjCDVCClientTests.m
+++ DevCycleTests/ObjC/ObjCDVCClientTests.m
@@ -14,43 +14,44 @@
 @implementation ObjcDVCClientTests
 
 - (void)testBuilderReturnsErrorIfNoEnvKey {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Builder returns error if no env key"];
     DVCUser *user = [DVCUser initializeWithUserId:@"my_user"];
     DVCClient *client = [DVCClient initialize:nil user:user options:nil onInitialized:^(NSError * _Nullable err) {
         XCTAssertNil(client);
         XCTAssertNotNil(err);
+        [expectation fulfill];
     }];
+    [self waitForExpectationsWithTimeout:5.0 handler:nil];
 }
 
 - (void)testBuilderReturnsErrorIfNoUser {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Builder returns error if no user"];
     DVCClient *client = [DVCClient initialize:@"my_env_key" user:nil options:nil onInitialized:^(NSError * _Nullable err) {
         XCTAssertNil(client);
         XCTAssertNotNil(err);
+        [expectation fulfill];
     }];
+    [self waitForExpectationsWithTimeout:5.0 handler:nil];
 }
 
 - (void)testBuilderCreatesClientWithUserAndEnvKey {
     DVCUser *user = [DVCUser initializeWithUserId:@"my_user"];
-    DVCClient *client = [DVCClient initialize:@"my_env_key" user:user options:nil onInitialized:^(NSError * _Nullable err) {
-        XCTAssertNil(err);
-        XCTAssertNotNil(client);
-    }];
+    DVCClient *client = [DVCClient initialize:@"my_env_key" user:user options:nil onInitialized:nil];
+    XCTAssertNotNil(client);
 }
 
 #pragma mark - Variable Tests
 
 - (void)testVariableIsCreated {
     DVCUser *user = [DVCUser initializeWithUserId:@"my_user"];
-    DVCClient *client = [DVCClient initialize:@"my_env_key" user:user options:nil onInitialized:^(NSError * _Nullable err) {
-        XCTAssertNil(err);
-        XCTAssertNotNil(client);
-        
-        DVCVariable *variable = [client stringVariableWithKey:@"my-key" defaultValue:@"default-value"];
-        XCTAssertNotNil(variable);
-        XCTAssertNil(variable.type);
-        XCTAssertNil(variable.evalReason);
-        XCTAssertEqual(variable.value, @"default-value");
-        XCTAssertEqual(variable.defaultValue, @"default-value");
-    }];
+    DVCClient *client = [DVCClient initialize:@"my_env_key" user:user options:nil onInitialized:nil];
+    XCTAssertNotNil(client);
+    DVCVariable *variable = [client stringVariableWithKey:@"my-key" defaultValue:@"default-value"];
+    XCTAssertNotNil(variable);
+    XCTAssertTrue([variable.type isEqualToString:@"String"]);
+    XCTAssertNil(variable.evalReason);
+    XCTAssertEqual(variable.value, @"default-value");
+    XCTAssertEqual(variable.defaultValue, @"default-value");
 }
 
 @end
