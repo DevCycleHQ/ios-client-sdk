f50a374ad2f7317ba01b65bec069407122130ef7
Parth Suthar <parth.suthar@taplytics.com>
2023-01-16 22:16:08 -0500

    [feat] add model identifier for osx

--- DevCycle/Models/PlatformDetails.swift
+++ DevCycle/Models/PlatformDetails.swift
@@ -9,6 +9,8 @@ import Foundation
 
 #if os(iOS)
 import UIKit
+#elseif os(OSX)
+import IOKit
 #endif
 
 struct PlatformDetails {
@@ -19,11 +21,23 @@ struct PlatformDetails {
     #elseif os(OSX)
     
     // TODO: figure out how to get this from macos: https://stackoverflow.com/questions/20070333/obtain-model-identifier-string-on-os-x
-    var deviceModel = "model"
+    var deviceModel = getModelIdentifier()
     var systemVersion: String { ProcessInfo.processInfo.operatingSystemVersionString }
     var systemName = "macos"
     #endif
     
     var sdkType = "mobile"
     var sdkVersion = "1.7.2"
+}
+
+func getModelIdentifier() -> String? {
+    let service = IOServiceGetMatchingService(kIOMasterPortDefault,
+                                              IOServiceMatching("IOPlatformExpertDevice"))
+    var modelIdentifier: String?
+    if let modelData = IORegistryEntryCreateCFProperty(service, "model" as CFString, kCFAllocatorDefault, 0).takeRetainedValue() as? Data {
+        modelIdentifier = String(data: modelData, encoding: .utf8)?.trimmingCharacters(in: .controlCharacters)
+    }
+
+    IOObjectRelease(service)
+    return modelIdentifier
 }
