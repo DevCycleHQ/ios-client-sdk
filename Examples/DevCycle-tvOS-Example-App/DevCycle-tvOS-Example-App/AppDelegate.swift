1ea546ffc093630136d024c7405cbb401dfb58a8
Jonathan Norris <jonathan@taplytics.com>
2023-01-26 11:40:06 -0500

    Adding tvOS Support (#139)
    * tvOS working, added example app
    
    * add Fastlane tvOS tests
    
    * split test into its own file
    
    * Update test for iPadOS
    
    * cleanup Podfile warnings on example apps
    
    Co-authored-by: Parth Suthar <parth.suthar@taplytics.com>

---
+++ Examples/DevCycle-tvOS-Example-App/DevCycle-tvOS-Example-App/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  DevCycle-tvOS-Example-App
+//
+
+import UIKit
+import DevCycle
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // create anonymous user
+        let user = try? DVCUser.builder()
+                               .isAnonymous(true)
+                               .build()
+        
+        // initialize DevCycle
+        if let user = user {
+            DevCycleManager.shared.initialize(user: user)
+        }
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+}
