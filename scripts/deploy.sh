73af6b144030e83c41d731d8ec6a1dc3a2534c0f
Jonathan Norris <jonathan@taplytics.com>
2023-02-02 11:53:21 -0500

    fix deploy.sh script building with watchOS simulators (#146)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,21 @@ if [[ -z "$DEVCYCLE_PROD_SLEUTH_API_TOKEN" ]]; then
     exit 1
 fi
 
+# fix watchOS simulators: https://github.com/CocoaPods/CocoaPods/issues/11558#issuecomment-1284573492
+echo "Fixing watchOS Simulators"
+xcrun simctl list --json > simulators.json
+watch_os_udids=$(jq '.devices[] | map(select(.name | contains("Watch"))) | .[].udid' simulators.json -r)
+paired_watch_udids=$(jq '.pairs | map(.watch) | .[].udid' simulators.json -r)
+
+while IFS= read -r udid; do
+    if [[ ! "${paired_watch_udids[*]}" =~ "${udid}" ]]; then
+        xcrun simctl delete ${udid}
+    fi
+done <<< "$watch_os_udids"
+rm simulators.json
+
+# pod deploy
+echo "Cocoapods publish"
 pod trunk push DevCycle.podspec --allow-warnings
 
 if [[ "$?" != 0 ]]; then
