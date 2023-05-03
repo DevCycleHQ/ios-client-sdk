b6b96dafc00368fe1c7ae9e57e6d41a2589e4474
Jason Salaber <jcsalaber@hotmail.com>
2023-05-03 15:40:04 -0400

    feat: added new scheme for macos

---
+++ DevCycle_macOS/DevCycle_macOS.h
@@ -0,0 +1,19 @@
+//
+//  DevCycle_macOS.h
+//  DevCycle_macOS
+//
+//  Created by Jason Salaber on 2023-05-03.
+//  Copyright © 2023 Taplytics. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DevCycle_macOS.
+FOUNDATION_EXPORT double DevCycle_macOSVersionNumber;
+
+//! Project version string for DevCycle_macOS.
+FOUNDATION_EXPORT const unsigned char DevCycle_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DevCycle_macOS/PublicHeader.h>
+
+
