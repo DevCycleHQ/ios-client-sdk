8ca677eb198ff67c2d38ba4c17c4b6a99b396b6c
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2024-02-14 14:02:08 -0500

    chore: update release workflows to trigger CocoaPod Publish workflow on new tag push

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 SHA="$(git rev-parse HEAD)"
-DEVCYCLE_PROD_SLEUTH_API_TOKEN="$(aws secretsmanager get-secret-value --secret-id=DEVCYCLE_PROD_SLEUTH_API_TOKEN | jq -r .SecretString )"
-
-# make sure we're able to track this deployment
-if [[ -z "$DEVCYCLE_PROD_SLEUTH_API_TOKEN" ]]; then
-    echo "Sleuth.io deployment tracking token not found. Aborting."
-    exit 1
-fi
 
 # fix watchOS simulators: https://github.com/CocoaPods/CocoaPods/issues/11558#issuecomment-1284573492
 echo "Fixing watchOS Simulators"
@@ -29,8 +22,3 @@ if [[ "$?" != 0 ]]; then
     echo "Publish failed. Aborting."
     exit 1
 fi
-
-curl -X POST \
-    -d api_key=$DEVCYCLE_PROD_SLEUTH_API_TOKEN \
-    -d environment=production \
-    -d sha=$SHA https://app.sleuth.io/api/1/deployments/taplytics/ios-client-sdk/register_deploy
