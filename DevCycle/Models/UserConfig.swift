538e19887143f671d6505a45aa5c4d8f8072b0c2
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2025-07-17 16:24:51 -0400

    chore: wrap openFeatureEvalReason in #if DEBUG flag to only be available for debug builds/testing

--- DevCycle/Models/UserConfig.swift
+++ DevCycle/Models/UserConfig.swift
@@ -233,9 +233,11 @@ public struct EvalReason {
         return EvalReason(reason: "DEFAULT", details: details)
     }
 
+    #if DEBUG
     public static func openFeatureEvalReason(reason: String) -> EvalReason {
         return EvalReason(reason: reason, details: "")
     }
+    #endif
 }
 
 public typealias EvalMetaData = [String: Any]
